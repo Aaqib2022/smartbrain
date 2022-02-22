5b5c458c18ec849e1a17e253c75fe9fa5c6acc85
Aaqib khan <aaqib.rashidd@gmail.com>
2022-02-22 11:08:05 +0000

    style added to FaceRecognition image

--- src/Components/FaceRecognition/FaceRecognition.js
+++ src/Components/FaceRecognition/FaceRecognition.js
@@ -3,8 +3,10 @@ import React from 'react';
 const FaceRecognition = ({ imageUrl }) => {
 	console.log(imageUrl)
 	return (
-		<div className="center">
-			<img alt="face detection" src={imageUrl} />
+		<div className="center ma">
+			<div className="absolute mt2"> 
+				<img alt="face detection" src={imageUrl} width="500px" height="auto" />
+			</div>
 		 </div>
 		);
 }
