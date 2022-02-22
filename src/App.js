e397aa41d31e5031f025af5a972ab3f0b9ae1404
Aaqib khan <aaqib.rashidd@gmail.com>
2022-02-22 10:20:41 +0000

    Clarifai API and FaceRecognition added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
+import Clarifai from 'clarifai';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
 import { particlesOptions } from './Constants.js';
 import Particles from "react-tsparticles";
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition'
 import './App.css';
 
+const app = new Clarifai.App({
+    apiKey: '63dd9e713bf8405082a77e48957ac212'
+});
 
 class App extends Component {
+
+    constructor () {
+      super();
+      this.state = {
+        input:''
+      }
+    }
+
+    onInputChange = (event) => {
+        console.log(event.target.value);
+    }
+
+    onButtonSubmit = () => {
+      console.log("clicked");
+      app.models.predict("a403429f2ddf4b49b307e318f00e528b" , "https://samples.clarifai.com/face-det.jpg").then(
+        function (response) {
+          console.log(response);
+        },
+        function(err) {
+          console.log(err)
+        });
+    }
 
   render() {
     return (
@@ -20,7 +47,11 @@ class App extends Component {
         <Navigation />
         <Logo />  
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition />
     
       </div>
     );
--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './ImageLinkForm.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 	return (
-		<div className="tc">
-			<p className="f3 "> 
+		<div>
+			<p className="f3 "> s
 			{'This Magic Brain will detect faces in your pictures'}
 			</p>
 			<div>
 				<div className="form center pa4 br3 shadow-5">
-					<input className="f4 pa2 w-70 center" type="text"/>
-					<button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"> detect </button>
+					<input className="f4 pa2 w-70 center" type="text" onChange = {onInputChange}/>
+					<button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple" onClick={onButtonSubmit}> detect </button>
 				</div>
 			</div>
 		 </div>
