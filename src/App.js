8ad3291b4e945f3dc74bd90ee13fa0dd8132c1fb
Aaqib khan <aaqib.rashidd@gmail.com>
2022-02-23 09:56:19 +0000

    onRouteChange functionality adjusted

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
     }
 
       onRouteChange = (route) => {
+        console.log(route);
         this.setState({route: route});
       }
 
@@ -67,18 +68,22 @@ class App extends Component {
           id="tsparticles"
           options={particlesOptions}
         />
-        <Navigation />
-        <Signin onRouteChange = {this.state.onRouteChange}/>
-        <Logo />  
-        <Rank />
-        <ImageLinkForm 
-          onInputChange={this.onInputChange} 
-          onButtonSubmit={this.onButtonSubmit}
-        />
-        <FaceRecognition 
-        box = {this.state.box}
-        imageUrl = {this.state.imageUrl}
-        />
+        <Navigation onRouteChange={this.onRouteChange}/>
+        { this.state.route === "Signin" 
+          ? <Signin onRouteChange = {this.onRouteChange}/>
+          : <div> 
+              <Logo />  
+              <Rank />
+            <ImageLinkForm 
+              onInputChange={this.onInputChange} 
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition 
+            box = {this.state.box}
+            imageUrl = {this.state.imageUrl}
+            />
+            </div>
+         }
     
       </div>
     );
